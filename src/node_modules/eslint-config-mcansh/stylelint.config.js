4440b8991cb65d9cb7ebf62a2e0053f0ec84a976
na0kiB <na0ki199823@gmail.com>
2022-05-09 23:08:26 +0900

    after copleated nextjs

---
+++ src/node_modules/eslint-config-mcansh/stylelint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  processors: ['stylelint-processor-styled-components'],
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-config-styled-components',
+    'stylelint-prettier/recommended',
+  ],
+  plugins: ['stylelint-prettier'],
+  rules: {
+    'prettier/prettier': true,
+  },
+};
