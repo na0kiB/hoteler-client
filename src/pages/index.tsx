820c54ee52148f8ffd0f265aff9c73af4c6de11c
na0kiB <na0ki199823@gmail.com>
2022-05-24 04:36:13 +0900

    end api by egrok

---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Error from "next/error";
+import type { GetServerSideProps, NextPage } from "next";
+
+type Post = {
+  id: number;
+  title: string;
+};
+
+type Props = {
+  posts: Post[];
+};
+
+const Home: NextPage<Props> = (props, { statusCode }) => {
+  if (statusCode) {
+    return <Error statusCode={statusCode} />;
+  }
+  return (
+    <div>
+      <h2>POSTの一覧</h2>
+      {props.posts.map((post, id) => (
+        <>
+          <li key={id}>{post.id}</li>
+          <li>{post.title}</li>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`);
+  const json = await res.json();
+  return {
+    props: {
+      posts: json.posts,
+    },
+  };
+};
+export default Home;
