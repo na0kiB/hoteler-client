60d0741d5abee3fe77a43ec4aa1d9c09279cba73
na0kiB <na0ki199823@gmail.com>
2022-07-09 15:35:54 +0900

    [fix]nodeの中身を整理してlocationを切り出した

--- src/components/s3ByForm.tsx
+++ src/components/s3ByForm.tsx
@@ -14,6 +14,7 @@ const onUpdaloadImage = async (event: React.ChangeEvent<HTMLInputElement>) => {
     formData.append(key, fields[key]);
   }
   formData.append("file", file);
+  console.log(formData);
 
   const ret = await fetch(S3DirectPost.url, {
     method: "POST",
@@ -22,20 +23,26 @@ const onUpdaloadImage = async (event: React.ChangeEvent<HTMLInputElement>) => {
     },
     body: formData,
   });
-  console.log(S3DirectPost.url);
+  console.log(ret);
   // https://hoteler-image.s3.ap-northeast-1.amazonaws.com
   const resText = await ret.text();
   console.log(resText);
+
   const resXML = await parseXML(resText);
   console.log(resXML);
+
+  const locationOfImage =
+    resXML.getElementsByTagName("Location")[0].childNodes[0].nodeValue;
+
+  console.log(locationOfImage);
+
   const key = await resXML.getElementsByTagName("Key")[0].childNodes[0]
     .nodeValue;
   console.log(key);
-  // return (
-  //   <>
-  //     <input type="file" onChange={onUpdaloadImage} />
-  //   </>
-  // );
+
+  return (
+    <>{locationOfImage && <img src={locationOfImage} className="h-32 m-4" />}</>
+  );
 };
 const parseXML = (text: string) =>
   new DOMParser().parseFromString(text, "application/xml");
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Error from "next/error";
 import type { GetServerSideProps, NextPage } from "next";
-import onUploadImage from "";
+
+import onUploadImage from "components/s3ByForm";
 
 type Post = {
   id: number;
@@ -11,42 +12,6 @@ type Post = {
 type Props = {
   posts: Post[];
 };
-
-// const onUploadImage = async (event: React.ChangeEvent<HTMLInputElement>) => {
-//   const res = await fetch(
-//     `${process.env.NEXT_PUBLIC_API_URL}/v1/presigned-url`
-//   );
-//   const S3DirectPost = await res.json();
-//   if (!event.target.files) return;
-//   const file = event.target.files[0];
-
-//   const fields = S3DirectPost.fields;
-//   const formData = new FormData();
-//   for (const key in fields) {
-//     formData.append(key, fields[key]);
-//   }
-//   formData.append("file", file);
-
-//   const ret = await fetch(S3DirectPost.url, {
-//     method: "POST",
-//     headers: {
-//       Accept: "multipart/form-data",
-//     },
-//     body: formData,
-//   });
-//   console.log(S3DirectPost.url);
-//   // https://hoteler-image.s3.ap-northeast-1.amazonaws.com
-//   const resText = await ret.text();
-//   console.log(resText);
-//   const resXML = await parseXML(resText);
-//   console.log(resXML);
-//   const key = await resXML.getElementsByTagName("Key")[0].childNodes[0]
-//     .nodeValue;
-//   console.log(key);
-// };
-
-// const parseXML = (text: string) =>
-//   new DOMParser().parseFromString(text, "application/xml");
 
 const Home: NextPage<Props> = (props, { statusCode }) => {
   if (statusCode) {
